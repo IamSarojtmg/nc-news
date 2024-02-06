29adc7d024b48e31de3e3ae5b2aedbf6bf6b0613
Saroj <saroj_tamang@aol.com>
2024-02-06 18:57:30 +0000

    individual articles

--- nc-news/src/utils/utils.js
+++ nc-news/src/utils/utils.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 
 const ncNewsAPI = axios.create({
-    baseURL : 'https://backend-project-vpy0.onrender.com/api'
-})
+  baseURL: "https://backend-project-vpy0.onrender.com/api",
+});
 
 export const getAllArticles = () => {
-    return ncNewsAPI.get('/articles').then((response) => {
-        return response.data
-    })
-}
+  return ncNewsAPI.get("/articles").then((response) => {
+    return response.data;
+  });
+};
+
+export const getArticleByID = (article_id) => {
+  return ncNewsAPI.get(`/articles/${article_id}`).then((res) => {
+    return res.data;
+  });
+};
